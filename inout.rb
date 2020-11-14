54fef320e9296edafcd9ed658da4146a767bf64a
CobaltSato <satocobalt@gmail.com>
2020-11-15 05:13:12 +0900

    index/exitを追加する

--- inout.rb
+++ inout.rb
@@ -1,33 +1,61 @@
 #! /usr/bin/ruby
 
+# ユーザのデータを記録するcsvファイルを扱うためのクラス
 class User
-  def initialize(csvFileObj)
-    @csvFileObj =  csvFileObj
+  def initialize(fname)
+    @fname = fname
   end
 
-  def writeUserRecordToCSV(name, email)
-    @csvFileObj.write("#{name}, #{email}\n")
+  # 新規ユーザをcsvに書き込む
+  def create(fields)
+    f = File.open(@fname, "a")
+    f.write("#{fields[:name]}, #{fields[:email]}\n")
+    f.close
   end
+
+  # すべてのユーザデータをcsv形式で返す
+  def all()
+    f = File.open(@fname, "r")
+    contents = f.read
+    f.close
+    return contents
+  end
+end
+
+# 名前とemailを受け付けて, csvに書き込む
+def create
+  qmark = "?"
+  name = "noname"
+  email = "nobody@invalid.com"
+  "name,email".split(',').each do |field|
+    eval "puts(field + qmark)"
+    STDOUT.flush
+    eval "#{field} = gets.to_s.chomp"
+  end
+  $user.create(name: name, email: email)
+end
+
+# すべてのユーザを表示する
+def index
+  puts $user.all()
 end
 
 # _main
-
-# Init
-csvFileObj = File.open('users.csv', "a")
-file = User.new(csvFileObj) 
-qmark = "?"
-$name = "noname"
-$email = "nobody@invalid.com"
-
-# Input
-"$name,$email".split(',').each do |field|
-  eval "puts(field + qmark)"
+$user = User.new("users.csv")
+while true
+  # コマンド名を受け付ける:
+  puts "Please input create, index, or exit"
   STDOUT.flush
-  eval "#{field} = gets.to_s.chomp"
-end
+  command = gets.to_s.chomp
 
-# Output  
-file.writeUserRecordToCSV($name, $email)
- 
-# End
-csvFileObj.close
+  # コマンドを実行する:
+  if command.eql?("create")
+    create
+  elsif command.eql?("index")
+    index
+  elsif command.eql?("exit")
+    break
+  else
+    puts "[ERROR] no such a command"
+  end
+end
