66680382b86b8a09786701f4db7c914b84bbef9d
CobaltSato <satocobalt@gmail.com>
2020-11-15 20:58:53 +0900

    userのform入力を書く

--- server.rb
+++ server.rb
@@ -1,5 +1,6 @@
 require "socket"
 require "erb"
+require "uri"
 
 port = 48002
 server = TCPServer.open(port)
@@ -36,19 +37,18 @@ def routes(url)
 end
 
 $id = 0
+$user = User.new("users.csv")
+$name = "noname"
+$email = "nobody@invalid.com"
 while true
   Thread.start(server.accept) do |socket|
-    p socket.peeraddr
-
     request = socket.gets
     p request
 
     method = request.split(' ')[0]
     url = request.split(' ')[1]
-    p url
 
     if method.eql?("GET")
-      user = User.new("users.csv")
       fname = routes(url.to_s)
       if !fname.nil?
         f = File.open(fname)
@@ -63,6 +63,41 @@ Connection: close
 
 #{content}
       EOF
+    end
+
+    if method.eql?("POST")
+      # content_lengthを取り出す
+      while req_header = socket.gets.chomp
+        break if req_header == ''
+        h = req_header.split(':')
+        if h[0].strip.downcase == 'content-length'
+          content_length = h[1].strip.to_i
+        end
+      end
+
+      # bodyを読み込む
+      if content_length != nil
+        params = socket.read(content_length).to_s.split(/=|&/)
+
+        if $name != params[1] && $email != params[3]
+          $name = params[1]
+          $email = params[3]
+          $user.create(name: $name, email: URI.decode($email))
+          puts "Input name: $name, email: $email"
+
+        f = File.open("onclick.html")
+        content = ERB.new(f.read).result(binding)
+        f.close
+      socket.write <<-EOF
+HTTP/1.1 200 OK
+Content-Type: text/html; charset=UTF-8
+Server: rserver
+Connection: close
+
+#{content}
+      EOF
+        end
+      end
     end
 
     socket.close
