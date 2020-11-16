860e4b7e93b2389e1826d818414c66821747f3c1
CobaltSato <satocobalt@gmail.com>
2020-11-16 12:57:49 +0900

    Routerの機能を抽象化する

--- server.rb
+++ server.rb
@@ -9,24 +9,82 @@ port = 48002
 server = TCPServer.open(port)
 
 ActiveRecord::Base.establish_connection(adapter: ADAPTER, database: DBFILE)
+
 class User < ActiveRecord::Base
 
 end
 
-def routes(url)
-  if url.eql?('/users') || url.eql?('/')
-    return "index.html.erb"
+class UsersController
+  def index
+    fname = "index.html.erb"
+    return content(fname)
   end
-  if url.include?('/users/')
-    $id = url['/users/'.length].to_i
-    return "show.html.erb"
+  def show
+    fname = "show.html.erb"
+    return content(fname)
   end
+  private
+  def content(fname)
+    f = File.open(fname)
+    content = ERB.new(f.read).result(binding)
+    f.close
+    return content
+  end
+end
+
+def routes(method, url, socket)
+  if method.eql?("GET")
+    if url.eql?('/users') || url.eql?('/')
+      return $usersController.index
+    end
+    if url.include?('/users/')
+      $id = url['/users/'.length].to_i
+      return $usersController.show
+    end
+  end # if method.eql?("GET")
+
+  if method.eql?("POST") && url.eql?('/onclick.html')
+    # content_lengthを取り出す
+    while req_header = socket.gets.chomp
+      break if req_header == ''
+      h = req_header.split(':')
+      if h[0].strip.downcase == 'content-length'
+        content_length = h[1].strip.to_i
+      end
+    end
+
+    # bodyを読み込む
+    if content_length != nil
+      params = socket.read(content_length).to_s.split(/=|&/)
+
+      if $name != params[1] && $email != params[3] && $hobby != params[5]
+        $name = params[1]
+        $email = params[3]
+        $hobby = params[5]
+        # $user.create(name: $name, email: URI.decode($email))
+        user = User.new
+        user.name  = $name
+        user.email = URI.decode($email)
+        user.hobby = $hobby
+        user.save
+        puts "Input name: $name, email: $email"
+
+        # ありがとうございました.と表示する:
+        f = File.open("onclick.html")
+        content = ERB.new(f.read).result(binding)
+        f.close
+        return content
+      end # if $name != params[1] && $email != params[3] && $hobby != params[5]
+    end # if content_length != nil
+  end # if method.eql?("POST")
 end
 
 $id = 0
 $name = "noname"
 $email = "nobody@invalid.com"
 $hobby = "nothing"
+$usersController = UsersController.new
+
 while true
   Thread.start(server.accept) do |socket|
     request = socket.gets
@@ -34,66 +92,15 @@ while true
     url = request.split(' ')[1]
     puts method, url
 
-    if method.eql?("GET")
-      # 対応するviewを表示する
-      fname = routes(url.to_s)
-      if !fname.nil?
-        f = File.open(fname)
-        content = ERB.new(f.read).result(binding)
-        f.close
-      end
-      socket.write <<-EOF
+    content = routes(method, url.to_s, socket)
+    socket.write <<-EOF
 HTTP/1.1 200 OK
 Content-Type: text/html; charset=UTF-8
 Server: rserver
 Connection: close
 
 #{content}
-      EOF
-    end
-
-    if method.eql?("POST")
-      # content_lengthを取り出す
-      while req_header = socket.gets.chomp
-        break if req_header == ''
-        h = req_header.split(':')
-        if h[0].strip.downcase == 'content-length'
-          content_length = h[1].strip.to_i
-        end
-      end
-
-      # bodyを読み込む
-      if content_length != nil
-        params = socket.read(content_length).to_s.split(/=|&/)
-
-        if $name != params[1] && $email != params[3] && $hobby != params[5]
-          $name = params[1]
-          $email = params[3]
-          $hobby = params[5]
-          # $user.create(name: $name, email: URI.decode($email))
-          user = User.new
-          user.name  = $name
-          user.email = URI.decode($email)
-          user.hobby = $hobby
-          user.save
-          puts "Input name: $name, email: $email"
-
-          # ありがとうございました.と表示する:
-          f = File.open("onclick.html")
-          content = ERB.new(f.read).result(binding)
-          f.close
-          socket.write <<-EOF
-HTTP/1.1 200 OK
-Content-Type: text/html; charset=UTF-8
-Server: rserver
-Connection: close
-
-#{content}
-      EOF
-        end # if $name != params[1] && $email != params[3] && $hobby != params[5]
-      end # if content_length != nil
-    end # if method.eql?("POST")
-
+EOF
     socket.close
   end
 end
